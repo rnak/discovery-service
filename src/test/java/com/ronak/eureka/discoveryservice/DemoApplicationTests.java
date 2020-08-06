2ec0216031d3c35ddf1cfcf6d504d6f13b91a078
Ronak Jain <rnakjain@gmail.com>
2020-08-06 11:36:12 +0530

    initial commit

---
+++ src/test/java/com/ronak/eureka/discoveryservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronak.eureka.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
